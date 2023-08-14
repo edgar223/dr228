d005393f932f693a3f70373d8afafb45bf084167
edgar223 <142250690+edgar223@users.noreply.github.com>
2023-08-14 23:14:59 +0300

    Update main.pyr

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
 import random
 a = random.randint(1, 9)
 print(a)
-
-# This is a sample Python script.
-
-# Press Shifhttps://github.com/edgar223/test22.gitt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#isd
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('edgar')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
